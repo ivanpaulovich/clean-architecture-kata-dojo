2635886df8c72b847b751e159ca8a6bd0390d439
ivanpaulovich <ivan@100loop.com>
2018-08-01 21:28:42 +0200

    final solution

--- kata-initial/src/Manga.WebApi/UseCases/Register/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Manga.WebApi.UseCases.Register
-{
-    public class RegisterRequest
-    {
-        public string Personnummer { get; set; }
-        public string Name { get; set; }
-        public double InitialAmount { get; set; }
-    }
-}
